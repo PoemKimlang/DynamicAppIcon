2e8753ca2a4e680d99957454b83bb0ff981c5c0e
Kimlang <poemkimlang010@gmail.com>
2025-03-01 21:44:05 +0700

    Initial Commit

---
+++ DynamicIconTests/DynamicIconTests.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicIconTests.swift
+//  DynamicIconTests
+//
+//  Created by Poem Kimlang on 1/3/25.
+//
+
+import Testing
+@testable import DynamicIcon
+
+struct DynamicIconTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
